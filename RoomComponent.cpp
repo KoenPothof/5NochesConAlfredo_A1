1a465fd0fa205c201be6e4d4984898820b86dd2f
jewiegel <j.wiegel@student.avans.nl>
2024-05-22 16:05:39 +0200

    changed RoomComponent slightly

--- RoomComponent.cpp
+++ RoomComponent.cpp
@@ -1,19 +1,9 @@
 #include "RoomComponent.h"
 
-RoomComponent::RoomComponent(int x1, int y1, int x2, int y2, int height)
+RoomComponent::RoomComponent(const float sideWallsWidth, const float frontWallsWidth)
 {
-    // Bottom wall
-    rectangles.push_back(std::make_shared<RectangleComponent>(0, true, x2 - x1, height));
-
-    // Top wall
-    rectangles.push_back(std::make_shared<RectangleComponent>(0, true, x2 - x1, height));
-
-    // Left wall
-    rectangles.push_back(std::make_shared<RectangleComponent>(90, true, y2 - y1, height));
-
-    // Right wall
-    rectangles.push_back(std::make_shared<RectangleComponent>(90, true, y2 - y1, height));
-
+    // Left and right walls
+    //rectangles.push_back(std::make_shared<RectangleComponent>(1, false, ));
 }
 
 RoomComponent::~RoomComponent()
--- RoomComponent.h
+++ RoomComponent.h
@@ -5,13 +5,15 @@
 #include <vector>
 #include <memory>
 
+const float ROOM_HEIGHT = 2.0f;
+
 class RoomComponent : public DrawComponent
 {
 private:
     std::vector<std::shared_ptr<RectangleComponent>> rectangles;
 
 public:
-    RoomComponent(int x1, int y1, int x2, int y2, int height);
+    RoomComponent(const float sideWallsWidth, const float frontWallsWidth);
     ~RoomComponent();
 
     virtual void draw() override;
