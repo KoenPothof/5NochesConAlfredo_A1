518fcc027d4f356d10961717011eb6831918d5d0
KaasKopjeThee <120020863+KaasKopjeThee@users.noreply.github.com>
2024-05-24 13:06:38 +0200

    Created map
    Map w all rooms named after the rooms on the design including the hallways

--- RoomComponent.cpp
+++ RoomComponent.cpp
@@ -13,9 +13,9 @@ RoomComponent::RoomComponent(const int sideWallsWidth, const int frontWallsWidth
     int y = 0;
     int z = 0;
 
-    rectangles.push_back(std::make_shared<RectangleComponent>(x, y, z, 0, false, frontWallsWidth, height, texture.setTexture(0, 0))); // front wall
+    rectangles.push_back(std::make_shared<RectangleComponent>(x - frontWallsWidth, y, z, 0, false, frontWallsWidth, height, texture.setTexture(0, 0))); // front wall
     rectangles.push_back(std::make_shared<RectangleComponent>(x - frontWallsWidth, y, z, 1, false, sideWallsWidth, height, texture.setTexture(0, 0))); // right wall
-    rectangles.push_back(std::make_shared<RectangleComponent>(x, y, z - sideWallsWidth, 0, false, frontWallsWidth, height, texture.setTexture(0, 0))); // back wall
+    rectangles.push_back(std::make_shared<RectangleComponent>(x - frontWallsWidth, y, z - sideWallsWidth, 0, false, frontWallsWidth, height, texture.setTexture(0, 0))); // back wall
     rectangles.push_back(std::make_shared<RectangleComponent>(x, y, z, 1, false, sideWallsWidth, height, texture.setTexture(0, 0))); // left wall
     rectangles.push_back(std::make_shared<RectangleComponent>(x, y, z, 1, true, frontWallsWidth, -1*sideWallsWidth, texture.setTexture(1, 0))); // floor
     rectangles.push_back(std::make_shared<RectangleComponent>(x, height, z, 1, true, frontWallsWidth, -1*sideWallsWidth, texture.setTexture(2, 0))); // ceiling
