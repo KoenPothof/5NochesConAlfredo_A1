5746374173aadc07037fdcfe318f9a4b5c7dc0a6
Wessel <w.roovers1@student.avans.nl>
2024-06-07 13:58:21 +0200

    Sound methods working

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,13 +1,12 @@
 #include "GameManager.h"
 #include "GameObject.h"
 #include "SecurityDoorComponent.h"
-#include "SoundComponent.h"
 
 #include <irrKlang.h>
 #pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
 
 irrklang::ISoundEngine* soundEngine;
-irrklang::ISound* sound;
+irrklang::ISound* soundPlay;
 
 GameManager::GameManager()
 {
@@ -61,15 +60,35 @@ bool GameManager::leftDoorClosed()
 void GameManager::rightDoorToggle()
 {
 	rightDoor->getComponent<SecurityDoorComponent>()->isClosed = !rightDoor->getComponent<SecurityDoorComponent>()->isClosed;
-	playSound();
+	if (rightDoor->getComponent<SecurityDoorComponent>()->isClosed)
+		playSound(DOOR_CLOSE);
+	else
+		playSound(DOOR_OPEN);
 }
 
 void GameManager::leftDoorToggle()
 {
 	leftDoor->getComponent<SecurityDoorComponent>()->isClosed = !leftDoor->getComponent<SecurityDoorComponent>()->isClosed;
-	playSound();
+	if (leftDoor->getComponent<SecurityDoorComponent>()->isClosed)
+		playSound(DOOR_CLOSE);
+	else
+		playSound(DOOR_OPEN);
 }
 
-void GameManager::playSound() {
-	sound = soundEngine->play2D("assets/sounds/musicWin.mp3", true, false, true);
+void GameManager::playSound(Sounds sound) {
+
+	switch (sound)
+	{
+		case DOOR_OPEN:
+			soundPlay = soundEngine->play2D("assets/sounds/doorOpen.mp3", false, false, true);
+			break;
+
+		case DOOR_CLOSE:
+			soundPlay = soundEngine->play2D("assets/sounds/doorClose.mp3", false, false, true);
+			break;
+
+		case WIN:
+			soundPlay = soundEngine->play2D("assets/sounds/musicWin.mp3", false, false, true);
+			break;
+	}
 }
